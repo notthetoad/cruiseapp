6afad3589bd5fe91b3c941591e6582924d07e0dd
Jakub Gruchalski <gruchalski.kuba@gmail.com>
2024-11-04 20:10:18 +0100

    wip integration test for cruise

--- server/server_test.go
+++ server/server_test.go
@@ -8,6 +8,7 @@ import (
 	"io"
 	"net/http"
 	"net/http/httptest"
+	"strconv"
 	"strings"
 	"testing"
 )
@@ -28,14 +29,18 @@ func prepBody(v any) (io.Reader, error) {
 
 }
 
+var currId int64
+var want = "fooland"
+
 func TestCreatePortIntegration(t *testing.T) {
-	expected := "fooland"
+	if testing.Short() {
+		t.Skip("Skipping create integration test")
+	}
 	ts := createTestServer()
-	_ = ts
 	defer ts.Close()
 
 	req := dto.CreatePortRequest{
-		Location: expected,
+		Location: want,
 	}
 	bodyReader, err := prepBody(req)
 	if err != nil {
@@ -51,14 +56,87 @@ func TestCreatePortIntegration(t *testing.T) {
 	_ = json.NewDecoder(res.Body).Decode(&port)
 	defer res.Body.Close()
 
-	fmt.Println(port)
 	if res.StatusCode != 201 {
 		t.Errorf("Expected 201, Got %d", res.StatusCode)
 	}
 	if !(port.Id > 0) {
 		t.Errorf("Id cannot be 0, Got: %d", port.Id)
 	}
-	if port.Location != expected {
-		t.Errorf("Expected %s, Got %s", expected, port.Location)
+	if port.Location != want {
+		t.Errorf("Expected %s, Got %s", want, port.Location)
 	}
+	currId = port.Id
+}
+
+func TestRetrievePortIntegration(t *testing.T) {
+	if testing.Short() {
+		t.Skip("Skipping retrieve integration test")
+	}
+
+	ts := createTestServer()
+	defer ts.Close()
+
+	id := strconv.Itoa(int(currId))
+	res, err := http.Get(ts.URL + "/port/" + id)
+	if err != nil {
+		t.Error(err)
+	}
+	var p model.Port
+	err = json.NewDecoder(res.Body).Decode(&p)
+	if err != nil {
+		t.Error(err)
+	}
+	defer res.Body.Close()
+
+	if p.Id != currId {
+		t.Errorf("Incorrect id. Want %d, Got %d", currId, p.Id)
+	}
+
+	if p.Location != want {
+		t.Errorf("Retrieved port incorrect. Want %s, Got %s", want, p.Location)
+	}
+}
+
+func TestUpdatePortIntegration(t *testing.T) {
+	if testing.Short() {
+		t.Skip("Skipping update integration test")
+	}
+
+	ts := createTestServer()
+	defer ts.Close()
+
+	body := dto.CreatePortRequest{
+		Location: "updated",
+	}
+	bodyReader, err := prepBody(body)
+	if err != nil {
+		t.Error(err)
+	}
+
+	id := strconv.Itoa(int(currId))
+	req := httptest.NewRequest(http.MethodPut, ts.URL+"/port/"+id, bodyReader)
+	// TODO finish test using the same methodology as in delete test
+
+}
+
+func TestDeletePortIntegration(t *testing.T) {
+	if testing.Short() {
+		t.Skip("Skipping delete integration test")
+	}
+
+	ts := createTestServer()
+	defer ts.Close()
+
+	id := strconv.Itoa(int(currId))
+	req, err := http.NewRequest(http.MethodDelete, ts.URL+"/port/"+id, nil)
+	if err != nil {
+		t.Error(err)
+	}
+	c := http.Client{}
+	res, err := c.Do(req)
+	if err != nil {
+		t.Error(err)
+	}
+	fmt.Println(res)
+	// TODO finish asserts
 }
